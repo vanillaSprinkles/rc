753de8c9885b30d0a54518fe0e37bc371986e707
vanillaSprinkles <vanillaSprinkles@users.noreply.github.com>
2024-10-20 06:09:31 +0000

    chore(bash_stuff/bscripts): minor improvements to quality of life; tmux > screen (#18)
    goodbye screen, thanks for all the fish! removal of seconds from status
    has me completly move over to tmux (tmux.conf's status now looks like
    what screen did in screen 4.9)

--- HOME/.bash_stuff/screen.bash
+++ HOME/.bash_stuff/screen.bash
@@ -1,29 +1,44 @@
 #!/bin/bash
 
 # screen
-SCREEN=/usr/bin/screen
-function screen () {
-  MSG=$( ${SCREEN} "${@}" )
-  if [[ "${MSG:0:25}" == "Cannot open your terminal" ]]; then
-      CMD="'screen ${@}'"
-      eval script /dev/null -c ${CMD} 
-  else
-      echo -e "${MSG}"
-  fi
-}
 
-alias sc='screen'
-#alias sN='screen -S'
-function sN () {
-  now=$(date "+%s.%3N")
-  screen -S ${1:-$now}
-}
-alias srx='screen -rx'
-alias rx='screen -rx'
-alias sls='screen -ls'
-alias scls='screen -ls'
-alias sdr='screen -D -R'
-alias sdrr='screen -D -RR'
-alias rxdr='screen -D -R'
-alias rxdrr='screen -D -RR'
+## screen deprecations in 5.0 (2024)
+## time function
+## seconds in status
+## 'load' is broken
+
+alias sN='echo tmux'
+alias scls='echo tmux'
+alias rx='echo tmux'
+alias screen='echo tmux'
+
+
+
+#_OFF_rip_2024_#
+#_OFF_rip_2024_#SCREEN=/usr/bin/screen
+#_OFF_rip_2024_#function screen () {
+#_OFF_rip_2024_#  MSG=$( ${SCREEN} "${@}" )
+#_OFF_rip_2024_#  if [[ "${MSG:0:25}" == "Cannot open your terminal" ]]; then
+#_OFF_rip_2024_#      CMD="'screen ${@}'"
+#_OFF_rip_2024_#      eval script /dev/null -c ${CMD} 
+#_OFF_rip_2024_#  else
+#_OFF_rip_2024_#      echo -e "${MSG}"
+#_OFF_rip_2024_#  fi
+#_OFF_rip_2024_#}
+#_OFF_rip_2024_#
+#_OFF_rip_2024_#alias sc='screen'
+#_OFF_rip_2024_##alias sN='screen -S'
+#_OFF_rip_2024_#function sN () {
+#_OFF_rip_2024_#  now=$(date "+%s.%3N")
+#_OFF_rip_2024_#  screen -S ${1:-$now}
+#_OFF_rip_2024_#}
+#_OFF_rip_2024_#alias srx='screen -rx'
+#_OFF_rip_2024_#alias rx='screen -rx'
+#_OFF_rip_2024_#alias sls='screen -ls'
+#_OFF_rip_2024_#alias scls='screen -ls'
+#_OFF_rip_2024_#alias sdr='screen -D -R'
+#_OFF_rip_2024_#alias sdrr='screen -D -RR'
+#_OFF_rip_2024_#alias rxdr='screen -D -R'
+#_OFF_rip_2024_#alias rxdrr='screen -D -RR'
+#_OFF_rip_2024_#
 
